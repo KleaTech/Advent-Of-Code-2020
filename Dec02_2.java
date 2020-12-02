06f513ace387a948156c76fb298ef5db8910dd91
Adam Bozzay <adam.bozzay.it@gmail.com>
2020-12-02 15:02:17 +0000

    Dec02_2 passed

--- Dec02_2.java
+++ Dec02_2.java
@@ -2,29 +2,27 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-class Dec02_1 {
+class Dec02_2 {
   public static void main(String[] args) throws IOException {
     var validCount = Files.lines(Paths.get("Dec02_1_input.txt"))
       .map(Entry::parse)
-      .filter(e -> {
-          var occurences = e.password.chars().filter(c -> c == e.character).count();
-          return occurences >= e.minOccurance && occurences <= e.maxOccurance;
-        })
+      .filter(e -> e.password.charAt(e.position1 - 1) == e.character && e.password.charAt(e.position2 - 1) != e.character || 
+                   e.password.charAt(e.position1 - 1) != e.character && e.password.charAt(e.position2 - 1) == e.character)
       .count();
     System.out.println(validCount);
   }
 
   static class Entry {
-    int minOccurance;
-    int maxOccurance;
+    int position1;
+    int position2;
     char character;
     String password;
 
     static Entry parse(String line) {
       return new Entry() {{
         var split = line.split("-|: | ");
-        minOccurance = Integer.parseInt(split[0]);
-        maxOccurance = Integer.parseInt(split[1]);
+        position1 = Integer.parseInt(split[0]);
+        position2 = Integer.parseInt(split[1]);
         character = split[2].charAt(0);
         password = split[3];
       }};
