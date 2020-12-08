8764734fd68bcd8a3bfd1e657cd6f010322b5390
Adam Bozzay <adam.bozzay.it@gmail.com>
2020-12-08 12:43:02 +0000

    Dec08_1 passed

---
+++ Dec08_1.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Dec08_1 {
+  public static void main(String[] args) throws IOException {
+    var lines = Files.lines(Paths.get("Dec08_1_input.txt")).collect(Collectors.toList());
+    var linesHit = new TreeSet<Integer>();
+    var acc = 0;
+    for (int pc=0; !linesHit.contains(pc); pc++) {
+      var value = Integer.parseInt(lines.get(pc).split(" ")[1]);
+      switch(lines.get(pc).split(" ")[0]) {
+        case "acc": acc += value; break;
+        case "jmp": pc += (value - 1); break;
+      }
+      linesHit.add(pc);
+    }
+    System.out.println(acc);
+  }
+}
